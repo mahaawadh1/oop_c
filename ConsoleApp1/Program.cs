a526b2d448c91e6ed1bcf3bea8785c4710944b7f
mahaawadh1 <mahaaaq897@gmail.com>
2024-04-15 09:51:40 +0300

    opp task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Student student = new Student();
+            student.Name = "maha salem";
+            student.Major = "Computer engineering";
+            student.GPA = 3.4;
+            student.PrintSummary();
+
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Major { get; set; }
+            public double GPA { get; set; }
+
+            public void PrintSummary()
+            {
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Major: {Major}");
+                Console.WriteLine($"GPA: {GPA:F2}");
+            }
+        }
+    }
+}
